89411bf437bc70ae923e9c59fc057d10dce34e32
Ray <linxray@gmail.com>
2015-07-12 14:51:23 +0800

    add vendor js

--- src/script/index_jsx.js
+++ src/script/index_jsx.js
@@ -1,3 +1,7 @@
-var React = require("react");
-var $ = require("jquery");
-React.render(<h1>helloWorld</h1>, $("#container").get(0));
+require(['style/index']);
+require(['script/vendor'], function(vendor) {
+  var React = vendor.react;
+  var container = document.getElementById("container");
+  React.render(<h1>helloWorld</h1>, container);
+
+});
---
+++ src/script/vendor.js
@@ -0,0 +1,7 @@
+require('imports?this=>window!modernizr');
+require('fastclick');
+
+module.exports = {
+  "foundation": require('imports?jQuery=jquery!foundation-sites'),
+  "react": require('react')
+};
