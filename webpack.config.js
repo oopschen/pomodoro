3147bb4d89863697a615d0b6c152f1ee3992e392
Ray <linxray@gmail.com>
2015-07-26 10:10:00 +0800

    bugfix prod defineplugin

--- webpack.config.js
+++ webpack.config.js
@@ -86,7 +86,7 @@ if (!isProd) {
   var plugins = [
     new webpack.DefinePlugin({
       'process.env': {
-        'NODE_ENV': process.env["NODE_ENV"]
+        'NODE_ENV': "\"" + process.env["NODE_ENV"] + "\""
       }
     }),
 
