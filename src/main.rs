79f4e0cc915b7fd14fa38aa1ab23710ad2cc8989
Ray <linxray@gmail.com>
2017-02-24 17:00:24 +0800

    add command line parse

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+#[macro_use]
+extern crate clap;
+
+fn main() {
+    use clap::App;
+
+    let cmd_yaml = load_yaml!("cmd.yml");
+    let cmd_matches = App::from_yaml(cmd_yaml).get_matches();
+    println!("break-time={}, long-break-time={}, work-time={}, threadhold={}",
+             cmd_matches.value_of("break").unwrap(),
+             cmd_matches.value_of("longbreak").unwrap(),
+             cmd_matches.value_of("work").unwrap(),
+             cmd_matches.value_of("longbreakthreadhold").unwrap()
+             );
+}
+
