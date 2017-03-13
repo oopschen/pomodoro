6b78b09f72045f230b7f3f02ab36a9a053083655
Ray <linxray@gmail.com>
2017-03-13 17:34:09 +0800

    add poll func

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,6 @@
 #[macro_use]
 extern crate clap;
-extern crate ncurses;
-extern crate timer;
-extern crate chrono;
+extern crate mio;
 mod pomodoro;
 
 use std::char;
@@ -12,18 +10,84 @@ use std::process::Command;
 use std::process::Stdio;
 use std::sync::mpsc::channel;
 use std::option::Option;
-use ncurses::*;
+use std::net::SocketAddr;
+use std::vec::Vec;
+use std::collections::VecDeque;
+use std::cell::Cell;
+use std::cell::RefCell;
+use clap::App;
+
+use mio::Events;
+use mio::Event;
+use mio::Poll;
+use mio::Token;
+use mio::Ready;
+use mio::PollOpt;
+use mio::Evented;
+use mio::tcp::TcpListener;
+use mio::tcp::TcpStream;
+
 use pomodoro::PSTATUS;
+use pomodoro::Pomodoro;
 
 macro_rules! errprint(
-    ($($arg:tt)*) => { {
-        let r = writeln!(&mut ::std::io::stderr(), $($arg)*);
-        r.expect("failed printing to stderr");
-    } }
-);
+    ($($arg:tt)*) => { 
+        {
+            let r = writeln!(&mut ::std::io::stderr(), $($arg)*);
+            r.expect("failed printing to stderr");
+        } 
+    }
+    );
+
+macro_rules! dereg(
+    ( $holder:expr, $i:ident, $poll:ident) => {
+        match $holder[$i-1] {
+            Some(ref h) => {
+                match h.hdl.get() {
+                    Some(hl) => {
+                        match $poll.deregister(hl) {
+                            Ok(_) => {},
+                            Err(e) => errprint!("deregister handler {} fail, {}", $i, e),
+                        }
+                    },
+                    None => {},
+                }
+
+            },
+            None => {},
+        }
+
+    }
+    );
+
+macro_rules! cleanup_holder(
+    ( $holder:expr ) => {
+        match $holder {
+            Some(ref h) => {
+                h.hdl.set(None);
+                *h.stream.borrow_mut() = None;
+            },
+            None => {},
+        }
+    }
+    );
 
 // work_ms, break_ms, lbreak_ms, lbreak_thread_hold
 type PTime = (u32, u32, u32, u8);
+
+struct PomodoroHolder<'a> {
+    pomo: Pomodoro,
+    addr: Cell<SocketAddr>,
+    msg: Cell<Option<String>>,
+    hdl: Cell<Option<&'a Evented>>,
+    stream: RefCell<Option<TcpStream>>,
+}
+
+enum PollAction {
+    EXIT(usize), // dereg, close
+    CONT, // do nothing continue
+    DEREG(usize), // deregister
+}
 
 const CMD_NEXT:u8 = 0;
 const CMD_RESET:u8 = 1;
@@ -34,8 +98,6 @@ const PMPT_STAGE: &'static str = "{sg}";
 const PMPT_STATUS: &'static str = "{st}";
 
 fn main() {
-    use clap::App;
-
     let cmd_yaml = load_yaml!("cmd.yml");
     let cmd_matches = App::from_yaml(cmd_yaml).get_matches();
     let notify_progs = match cmd_matches.value_of("notifycmd") {
@@ -114,24 +176,103 @@ fn main() {
     run_pomodoro((wms, bms, lbms, lb_thold), notify_progs, host, port, maxp);
 }
 
-fn run_pomodoro(time_args: PTime, notify_progs: &str, host: &str, port: u16, maxp: u16) {
-    // child thread is responsible for pomodoro logic
-    // main thread is responsible for listen input from keyboard and output
-    // 
-    // new channel
-    // clone  tx for chlid timer
-    // spawn a thread run pomodoro 
-     
-    // init term
-    initscr();
-    noecho();
-    print_prompt();
-     
-    let (tx_input, rx) = channel::<u8>();
-    let tx_timer = tx_input.clone();
-    let notify_progs_clone = String::from(notify_progs);
+fn parse_listen_address(host: &str, port: u16) -> SocketAddr {
+    format!("{}:{}", host, port).parse().unwrap()
+}
 
-    let pomodoro_thread = thread::spawn(move || {
+// run pomodoro 
+//  init array for datastore
+//  init avail index 
+//  listen on host and port
+//      accept
+//      if connect:
+//          get next index
+//          if null:
+//              write msg
+//              close
+//      else if read:
+//          read to eof
+//          compare cmd do jobs
+fn run_pomodoro(time_args: PTime, notify_progs: &str, host: &str, port: u16, maxp: u16) {
+    let sock_addr = parse_listen_address(host, port);
+    let mut holder_arr: Vec<Option<PomodoroHolder>> = Vec::new();
+    let mut avail_index: VecDeque<u16> = VecDeque::with_capacity(maxp as usize);
+    for _ in 0..maxp {
+        holder_arr.push(None);
+        /*
+           holder_arr.push(PomodoroHolder {
+           pomo: Pomodoro::new(time_args.0, time_args.1, time_args.2, time_args.3),
+           addr: Cell::new(sock_addr),
+           msg: RefCell::new("".to_string()),
+           });
+           */
+    }
+
+    let listener = match TcpListener::bind(&sock_addr) {
+        Ok(v) => v,
+        Err(e) => {
+            errprint!("can not listen to {}:{}, {}", host, port, e);
+            return;
+        }
+
+    };
+
+    let mut evts = Events::with_capacity(maxp as usize);
+
+    let poll = match Poll::new() {
+        Ok(p) => p,
+        Err(e) => {
+            errprint!("create poll error, {}", e);
+            return;
+        }
+    };
+
+    let server_token = 0;
+
+    match poll.register(&listener, Token(server_token), Ready::readable()|Ready::writable(), PollOpt::level()) {
+        Ok(_) => {},
+        Err(e) => {
+            errprint!("register listener fail, {}", e);
+            return;
+        }
+    };
+
+    loop {
+        let num = match poll.poll(&mut evts, None) {
+            Ok(n) => n,
+            Err(e) => {
+                errprint!("poll error, {}", e);
+                continue;
+            }
+        };
+
+        for i in 0..num {
+            match evts.get(i) {
+                None => continue,
+                Some(evt) => {
+                    match dispatch_event(time_args, &evt, &mut holder_arr, &mut avail_index) {
+                        PollAction::EXIT(inx) => {
+                            dereg!(&holder_arr, inx, poll);
+                            cleanup_holder!(holder_arr[inx]);
+                        },
+
+                        PollAction::CONT => {},
+
+                        PollAction::DEREG(inx) => {
+                            dereg!(&holder_arr, inx, poll);
+                        },
+                    }
+                }
+            }
+        }
+
+    }
+    /*
+       let (tx_input, rx) = channel::<u8>();
+       let tx_timer = tx_input.clone();
+       let notify_progs_clone = String::from(notify_progs);
+
+       let pomodoro_thread = thread::spawn(move || {
         // new timer 
         // new pomodoro
         // listen on channel for cmd:
@@ -154,138 +295,138 @@ fn run_pomodoro(time_args: PTime, notify_progs: &str, host: &str, port: u16, max
         //          clean it
         //      break
         let tr = timer::MessageTimer::new(tx_timer);
-        let pomodo = pomodoro::Pomodoro::new(time_args.0, time_args.1, time_args.2, time_args.3);
+        let pomodo = pomodoro::;
         let mut hdl: Option<timer::Guard> = Option::None;
 
         loop {
-            let cmd = rx.recv().unwrap();
-            // clean up timer
-            if let Some(v) = hdl {
-                drop(v);
-                hdl = Option::None;
-            }
+        let cmd = rx.recv().unwrap();
+    // clean up timer
+    if let Some(v) = hdl {
+    drop(v);
+    hdl = Option::None;
+    }
 
             // response to command
             match cmd {
-                CMD_NEXT => {
-                    let st = pomodo.next_step(); 
-                    match st {
-                        PSTATUS::StartWork | PSTATUS::StartBreak | PSTATUS::LStartBreak => {
-                            let (sg, sta) = match st {
-                                PSTATUS::StartWork => ("Work", "Started"),
-                                PSTATUS::StartBreak => ("Break", "Started"),
-                                PSTATUS::LStartBreak => ("Long-Break", "Started"),
-                                _ => ("Unknown", "Undefined")
+            CMD_NEXT => {
+            let st = pomodo.next_step(); 
+            match st {
+            PSTATUS::StartWork | PSTATUS::StartBreak | PSTATUS::LStartBreak => {
+            let (sg, sta) = match st {
+            PSTATUS::StartWork => ("Work", "Started"),
+            PSTATUS::StartBreak => ("Break", "Started"),
+            PSTATUS::LStartBreak => ("Long-Break", "Started"),
+            _ => ("Unknown", "Undefined")
 
-                            };
+            };
 
-                            hdl = Option::Some(tr.schedule_with_delay(
-                                    chrono::Duration::milliseconds(pomodo.get_ms(st) as i64),
-                                    CMD_TIMEOUT));
+            hdl = Option::Some(tr.schedule_with_delay(
+            chrono::Duration::milliseconds(pomodo.get_ms(st) as i64),
+            CMD_TIMEOUT));
 
-                            run_notify_command(notify_progs_clone.replace(PMPT_STAGE, sg).replace(PMPT_STATUS, sta));
-                        },
+            run_notify_command(notify_progs_clone.replace(PMPT_STAGE, sg).replace(PMPT_STATUS, sta));
+            },
 
-                        PSTATUS::EndWork => {
-                            run_notify_command(notify_progs_clone.replace(PMPT_STAGE, "Work").replace(PMPT_STATUS, "Stopped"));
-                        },
+            PSTATUS::EndWork => {
+            run_notify_command(notify_progs_clone.replace(PMPT_STAGE, "Work").replace(PMPT_STATUS, "Stopped"));
+            },
 
-                        PSTATUS::EndBreak => {
-                            run_notify_command(notify_progs_clone.replace(PMPT_STAGE, "Break").replace(PMPT_STATUS, "Stopped"));
-                        },
+            PSTATUS::EndBreak => {
+            run_notify_command(notify_progs_clone.replace(PMPT_STAGE, "Break").replace(PMPT_STATUS, "Stopped"));
+            },
 
-                        PSTATUS::LEndBreak => {
-                            run_notify_command(notify_progs_clone.replace(PMPT_STAGE, "Long-Break").replace(PMPT_STATUS, "Stopped"));
-                        },
+            PSTATUS::LEndBreak => {
+            run_notify_command(notify_progs_clone.replace(PMPT_STAGE, "Long-Break").replace(PMPT_STATUS, "Stopped"));
+            },
 
-                        _ => {
-                            run_notify_command(notify_progs_clone.replace(PMPT_STAGE, "Unknow").replace(PMPT_STATUS, "Undefined"));
-                        },
-                    }
+    _ => {
+        run_notify_command(notify_progs_clone.replace(PMPT_STAGE, "Unknow").replace(PMPT_STATUS, "Undefined"));
+    },
+    }
+},
+
+    CMD_QUIT => break,
+
+    CMD_TIMEOUT => {
+        match pomodo.status() {
+            PSTATUS::StartWork | PSTATUS::StartBreak | PSTATUS::LStartBreak => {},
+            _ => continue,
+        };
+
+        let st = pomodo.next_step();
+        // call notify progs
+        if "" != notify_progs_clone {
+            match st {
+                PSTATUS::EndWork => {
+                    run_notify_command(notify_progs_clone.replace(PMPT_STAGE, "Work").replace(PMPT_STATUS, "End"));
                 },
 
-                CMD_QUIT => break,
-
-                CMD_TIMEOUT => {
-                    match pomodo.status() {
-                        PSTATUS::StartWork | PSTATUS::StartBreak | PSTATUS::LStartBreak => {},
-                        _ => continue,
-                    };
-
-                    let st = pomodo.next_step();
-                    // call notify progs
-                    if "" != notify_progs_clone {
-                        match st {
-                            PSTATUS::EndWork => {
-                                run_notify_command(notify_progs_clone.replace(PMPT_STAGE, "Work").replace(PMPT_STATUS, "End"));
-                            },
-
-                            PSTATUS::EndBreak => {
-                                run_notify_command(notify_progs_clone.replace(PMPT_STAGE, "Break").replace(PMPT_STATUS, "End"));
-                            },
-
-                            PSTATUS::LEndBreak => {
-                                run_notify_command(notify_progs_clone.replace(PMPT_STAGE, "Long-Break").replace(PMPT_STATUS, "End"));
-                            },
-
-                            _ => {}
-                        }
-                    }
+                PSTATUS::EndBreak => {
+                    run_notify_command(notify_progs_clone.replace(PMPT_STAGE, "Break").replace(PMPT_STATUS, "End"));
                 },
 
-                CMD_RESET => {
-                    pomodo.reset();
-                    run_notify_command(notify_progs_clone.replace(PMPT_STAGE, "Reset").replace(PMPT_STATUS, "Done"));
+                PSTATUS::LEndBreak => {
+                    run_notify_command(notify_progs_clone.replace(PMPT_STAGE, "Long-Break").replace(PMPT_STATUS, "End"));
                 },
 
-                _ => continue,
+                _ => {}
             }
         }
-    });
-    // end thread
+    },
 
-    // listen on char input
-    loop {
-        let cr = getch();
+    CMD_RESET => {
+        pomodo.reset();
+        run_notify_command(notify_progs_clone.replace(PMPT_STAGE, "Reset").replace(PMPT_STATUS, "Done"));
+    },
 
-        if ERR == cr {
+    _ => continue,
+    }
+}
+});
+// end thread
+
+// listen on char input
+loop {
+    let cr = getch();
+
+    if ERR == cr {
+        clear_screen();
+        print_help(time_args);
+        continue;
+    }
+
+    let char_str = match char::from_u32(cr as u32) {
+        Some(ch) => ch,
+        None => {
             clear_screen();
             print_help(time_args);
             continue;
         }
 
-        let char_str = match char::from_u32(cr as u32) {
-            Some(ch) => ch,
-            None => {
-                clear_screen();
-                print_help(time_args);
-                continue;
-            }
+    };
 
-        };
+    // print to terminal
+    printw(format!("{}", char_str).as_ref());
 
-        // print to terminal
-        printw(format!("{}", char_str).as_ref());
+    match char_str {
+        'q' | 'Q' => {
+            tx_input.send(CMD_QUIT).unwrap();
+            pomodoro_thread.join().unwrap();
+            break;
+        },
 
-        match char_str {
-            'q' | 'Q' => {
-                tx_input.send(CMD_QUIT).unwrap();
-                pomodoro_thread.join().unwrap();
-                break;
-            },
+        'n' | 'N' => tx_input.send(CMD_NEXT).unwrap(),
+        'r' | 'R' => tx_input.send(CMD_RESET).unwrap(),
 
-            'n' | 'N' => tx_input.send(CMD_NEXT).unwrap(),
-            'r' | 'R' => tx_input.send(CMD_RESET).unwrap(),
-
-            _ => {
-                clear_screen();
-                print_help(time_args);
-            }
+        _ => {
+            clear_screen();
+            print_help(time_args);
         }
     }
-
-    endwin();
 }
+
+}
+
 
 fn run_notify_command(progs: String) {
     let mut splits_by_space = progs.split_whitespace();
@@ -303,26 +444,34 @@ fn run_notify_command(progs: String) {
     }
 
     cmd_builder.spawn().unwrap();
+    */
 }
 
-fn print_prompt() {
-    printw(format!("Command me[n/N/r/R/q/Q]:").as_ref());
-    refresh();
-}
+/*
+   fn print_prompt() {
+   printw(format!("Command me[n/N/r/R/q/Q]:").as_ref());
+   refresh();
+   }
 
-fn print_help(time_args: PTime) {
-    printw(format!("\
-Pomodoro runs a long break time of {} msecs after {} times work time of {} msecs which followd by a break time of {} msecs.
-Help list:
-    n/N\t\t\tGo to the next step
-    r/R\t\t\tReset the pomodoro
-    q/Q\t\t\tQuit the programs\n",
-    time_args.2, time_args.3, time_args.0, time_args.1).as_ref());
-    refresh();
-    print_prompt();
-}
+   fn print_help(time_args: PTime) {
+   printw(format!("\
+   Pomodoro runs a long break time of {} msecs after {} times work time of {} msecs which followd by a break time of {} msecs.
+   Help list:
+   n/N\t\t\tGo to the next step
+   r/R\t\t\tReset the pomodoro
+   q/Q\t\t\tQuit the programs\n",
+   time_args.2, time_args.3, time_args.0, time_args.1).as_ref());
+   refresh();
+   print_prompt();
+   }
 
-fn clear_screen() {
-    clear();
-    refresh();
+   fn clear_screen() {
+   clear();
+   refresh();
+   }
+   */
+
+fn dispatch_event(time_args: PTime, evt: &Event, holders: &mut Vec<Option<PomodoroHolder>>, avails: &mut VecDeque<u16>) -> PollAction {
+    PollAction::CONT
+
 }
